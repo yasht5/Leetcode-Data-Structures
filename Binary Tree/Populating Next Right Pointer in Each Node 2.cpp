e4da70713f90da62e7fc0abb8b5213d547f0267c
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-03-30 19:16:39 +0530

    Create Populating Next Right Pointer in Each Node 2.cpp

---
+++ Binary Tree/Populating Next Right Pointer in Each Node 2.cpp
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)
+        {
+            return NULL;
+        }
+        Node *prev = NULL;
+        Node *head = NULL;
+        Node *curr = root;
+        while(curr)
+        {
+            while(curr)
+            {
+                if(curr->left)
+                {
+                    if(prev)
+                    {
+                        prev->next = curr->left;
+                    }
+                    else
+                    {
+                        head = curr->left;
+                    }
+                    prev = curr->left;
+                }
+                if(curr->right)
+                {
+                    if(prev)
+                    {
+                        prev->next = curr->right;   
+                    }
+                    else
+                    {
+                        head = curr->right;
+                    }
+                    prev = curr->right;
+                }
+                curr = curr->next;
+            }
+            curr = head;
+            head = NULL;
+            prev = NULL;
+        }
+        return root;
+    }
+};
